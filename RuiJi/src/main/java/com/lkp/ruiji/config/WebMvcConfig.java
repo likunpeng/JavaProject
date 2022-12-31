a494d1694bd0d1cbae1d01bb70cba0f2cf2e63ef
likunpeng <360315247@qq.com>
2023-01-01 00:22:30 +0800

    解决静态资源映射失败问题

--- RuiJi/src/main/java/com/lkp/ruiji/config/WebMvcConfig.java
+++ RuiJi/src/main/java/com/lkp/ruiji/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.lkp.ruiji.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 @Slf4j
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     /**
      * 建立映射
