29c5082d594ddae4ff85ebb16fff4780d44c3e74
likunpeng <360315247@qq.com>
2023-01-01 00:03:39 +0800

    前端资源添加

--- RuiJi/src/main/java/com/lkp/ruiji/RuiJiApplication.java
+++ RuiJi/src/main/java/com/lkp/ruiji/RuiJiApplication.java
@@ -1,6 +1,15 @@
 package com.lkp.ruiji;
 
+import com.sun.jndi.url.rmi.rmiURLContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
+@SpringBootApplication
 public class RuiJiApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(RuiJiApplication.class, args);
+        log.info("项目启动成功~~~~~~~~~~~~");
+    }
 }
---
+++ RuiJi/src/main/java/com/lkp/ruiji/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.lkp.ruiji.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+@Slf4j
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 建立映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("建立静态映射！！！！！！！！！！！！！！！");
+//        super.addResourceHandlers(registry);
+        registry.addResourceHandler("backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("front/**").addResourceLocations("classpath:/front/");
+    }
+}
